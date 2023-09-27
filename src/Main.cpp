92c68a645e4848cf324de5e08ad237e5629d85db
Xero kun <elo48858@gmail.com>
2023-09-27 10:47:47 +0100

    refactor: Moved title display feature to separate function

--- src/Main.cpp
+++ src/Main.cpp
@@ -10,14 +10,19 @@ void triangleArea(), trapeziumArea();
 void squareArea(), squarePerimeter();
 void lineBreak();
 
+void displayTitle() {
+    cout <<"WELCOME TO THE MENSURATION (2D SHAPES) CALCULATOR!" << endl;
+    printf("--------------------------------------------------");
+    cout <<"\nThe available shapes are:" << endl;
+    cout << " \n\t1. Rectangle \n\t2. Parallelogram \n\t3. Triangle \n\t4. Trapezium \n\t5. Square " << endl;
+    cout <<"\n Enter the shape's number to continue: ";
+}
+
 
 int main(){
-   int shape, parameter;
-   cout <<"WELCOME TO THE MENSURATION (2D SHAPES) CALCULATOR!" << endl;
-   printf("--------------------------------------------------");
-   cout <<"\nThe available shapes are:" << endl;
-   cout << " \n\t1. Rectangle \n\t2. Parallelogram \n\t3. Triangle \n\t4. Trapezium \n\t5. Square " << endl;
-   cout <<"\n Enter the shape's number to continue: ";
+    displayTitle();
+
+    int shape, parameter;   
     cin >> shape;
     cin.clear();
 

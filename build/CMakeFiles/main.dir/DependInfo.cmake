459d73f7656ad545388ab28bdbdcee1baa309c23
Xero kun <elo48858@gmail.com>
2023-09-27 11:27:39 +0100

    feat(build): Setup cmake build chain

---
+++ build/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/elo48/OneDrive/Documents/C and C++ Programs/ricky-ultimate/cpp-simple-mensuration-calculator/src/display/display.cpp" "CMakeFiles/main.dir/src/display/display.cpp.obj" "gcc" "CMakeFiles/main.dir/src/display/display.cpp.obj.d"
+  "C:/Users/elo48/OneDrive/Documents/C and C++ Programs/ricky-ultimate/cpp-simple-mensuration-calculator/src/main.cpp" "CMakeFiles/main.dir/src/main.cpp.obj" "gcc" "CMakeFiles/main.dir/src/main.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
